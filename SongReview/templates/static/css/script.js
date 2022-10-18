af24e86cac94bbe51f15a7a9a1ae34392533f586
jtran978 <103219803+jtran978@users.noreply.github.com>
2022-10-18 11:10:26 -0400

    Add files via upload

---
+++ SongReview/templates/static/css/script.js
@@ -0,0 +1,10 @@
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+const form = document.querySelector("#form");
+const switchs = document.querySelector(".switch");
+
+function tab2(){
+    form.style.marginLeft = "-100%";
+    login.style.background = "none";
+    signup.style.background = "background: linear-gradient(45deg,#00d5fc, #046af6);";
+}
